306fe85c777d17026eddba97b4b2c8522d82a296
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-07-12 13:33:32 -0400

    feat: temporary look of homepage

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 /* EXPRESS SESSION */
 const sess = {
