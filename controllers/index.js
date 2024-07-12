d562908a9dec5ccd84b761ec294f73a68f00ee5e
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-07-12 17:01:16 -0400

    feat: clean up main page look and nav look

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,11 @@ const router = require("express").Router();
 
 /* ROUTES */
 const homeRoutes = require("./home-routes.js");
-// const locationRoutes = require("./location-routes.js");
+const locationRoutes = require("./location-routes.js");
 //const apiRoutes = require("./api");
 
 router.use("/", homeRoutes);
-// router.use("/locations", locationRoutes);
+router.use("/locations", locationRoutes);
 //router.use("/api", apiRoutes);
 
 /* EXPORTS */
