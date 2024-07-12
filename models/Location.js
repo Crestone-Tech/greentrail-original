c96e40686f1ecf4a7b6b3fe3fd1cd84ff99a43f6
Violet Stanziani <stanzianiviolet@gmail.com>
2024-07-12 12:36:15 -0400

    Added extension classes country, province, city

--- models/Location.js
+++ models/Location.js
@@ -1,6 +1,7 @@
 const {Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+
 class Location extends Model{}
 
 Location.init(
@@ -11,10 +12,18 @@ Location.init(
             primaryKey: true,
             autoIncrement: true,
         },
-        location_name: {
+        country: {
             type: DataTypes.STRING,
             allowNull: false,
         },
+        province: {
+            type:DataTypes.STRING,
+            allowNull: false,
+        },
+        city:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
     },
     {
         sequelize, 
@@ -22,7 +31,114 @@ Location.init(
         freezeTableName: true,
         underscored: true,
         modelName: 'location'
-    }
+    },
 );
+// Subclasses of location, 
+//Country,
+//Province/state
+//Town/City
+
+class Country extends Location{}
+
+Country.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true, 
+            autoIncrement: true,
+        },
+        country_name:{ 
+            type: DataTypes.STRING,
+            allowNull: false,
+            verification:{
+                isAlpha: true,
+            }
+
+        },
+    }, 
+    {
+        sequelize, 
+        tiemstamps: false, 
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'country'
+
+    }
+
+);
+
+class Province extends Location{}
+
+Province.init(
+    {
+        id: {
+            type:DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true, 
+            autoIncrement: true,
+        },
+        province_name: {
+           type: DataTypes.STRING, 
+           allowNull: false,
+           verification: {
+            isAlpha: true,
+           }
+        },
+        country_id:{
+            type: DataTypes.INTEGER,
+            unique: false,
+            references:{
+                model: 'country',
+                key: 'id'
+            },
+
+        },
+    },
+    {
+        sequelize, 
+        tiemstamps: false, 
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'province'
+    },
+);
+
+class Town extends Location{}
+
+Town.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        city_name:{
+            type: DataTypes.STRING,
+            verification:{
+                isAlpha: true,
+            },
+        },
+        country_id: {
+            type: DataTypes.INTEGER,
+            unique: false,
+            references: {
+                model: 'country', 
+                key: 'id'
+            }
+        },
+        province_key:{
+            type: DataTypes.INTEGER,
+            unique: false,
+            references: {
+                model: 'province',
+                key:'id'
+            }
+        }
+    },
+);
+
+
 
 module.exports = Location;
