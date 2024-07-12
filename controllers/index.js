3b97494d76b9da29856dc99a68de53ab63f2465c
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-07-12 17:53:59 -0400

    feat: add community page

--- controllers/index.js
+++ controllers/index.js
@@ -4,10 +4,12 @@ const router = require("express").Router();
 /* ROUTES */
 const homeRoutes = require("./home-routes.js");
 const locationRoutes = require("./location-routes.js");
+const communityRoutes = require("./community-routes.js");
 //const apiRoutes = require("./api");
 
 router.use("/", homeRoutes);
 router.use("/locations", locationRoutes);
+router.use("/community", communityRoutes);
 //router.use("/api", apiRoutes);
 
 /* EXPORTS */
