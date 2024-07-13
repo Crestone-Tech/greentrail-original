f60e10a79264759824be71e6e31e300361538bdd
cuhrmunken <elliott.davis@crestonetech.com>
2024-07-12 21:54:53 -0700

    adds Mexico states

--- seeds/province-seeds.js
+++ seeds/province-seeds.js
@@ -1,4 +1,4 @@
-const { Province } = require('../models/Location');
+const { Province } = require("../models/Location");
 
 const provinceData = [
   {
@@ -13,7 +13,6 @@ const provinceData = [
     province_name: "Manitoba",
     country_id: 1,
   },
-
   {
     province_name: "New Brunswick",
     country_id: 1,
@@ -53,6 +52,134 @@ const provinceData = [
   {
     province_name: "Northwest Territory",
     country_id: 1,
+  },
+  {
+    province_name: "Aguascalientes",
+    country_id: 2,
+  },
+  {
+    province_name: "Baja California",
+    country_id: 2,
+  },
+  {
+    province_name: "Baja California Sur",
+    country_id: 2,
+  },
+  {
+    province_name: "Campeche",
+    country_id: 2,
+  },
+  {
+    province_name: "Chiapas",
+    country_id: 2,
+  },
+  {
+    province_name: "Chihuahua",
+    country_id: 2,
+  },
+  {
+    province_name: "Coahuila",
+    country_id: 2,
+  },
+  {
+    province_name: "Colima",
+    country_id: 2,
+  },
+  {
+    province_name: "Durango",
+    country_id: 2,
+  },
+  {
+    province_name: "Guanajuato",
+    country_id: 2,
+  },
+  {
+    province_name: "Guerrero",
+    country_id: 2,
+  },
+  {
+    province_name: "Hidalgo",
+    country_id: 2,
+  },
+  {
+    province_name: "Jalisco",
+    country_id: 2,
+  },
+  {
+    province_name: "México",
+    country_id: 2,
+  },
+  {
+    province_name: "Mexico City",
+    country_id: 2,
+  },
+  {
+    province_name: "Michoacán",
+    country_id: 2,
+  },
+  {
+    province_name: "Morelos",
+    country_id: 2,
+  },
+  {
+    province_name: "Nayarit",
+    country_id: 2,
+  },
+  {
+    province_name: "Nuevo León",
+    country_id: 2,
+  },
+  {
+    province_name: "Oaxaca",
+    country_id: 2,
+  },
+  {
+    province_name: "Puebla",
+    country_id: 2,
+  },
+  {
+    province_name: "Querétaro",
+    country_id: 2,
+  },
+  {
+    province_name: "Quintana Roo",
+    country_id: 2,
+  },
+  {
+    province_name: "San Luis Potosí",
+    country_id: 2,
+  },
+  {
+    province_name: "Sinaloa",
+    country_id: 2,
+  },
+  {
+    province_name: "Sonora",
+    country_id: 2,
+  },
+  {
+    province_name: "Tabasco",
+    country_id: 2,
+  },
+  {
+    province_name: "Tamaulipas",
+    country_id: 2,
+  },
+  {
+    province_name: "Tlaxcala",
+    country_id: 2,
+  },
+  {
+    province_name: "Veracruz",
+    country_id: 2,
+  },
+  {
+    province_name: "Yucatán",
+    country_id: 2,
+  },
+  {
+    province_name: "Zacatecas",
+    country_id: 2,
   },
 ];
 
