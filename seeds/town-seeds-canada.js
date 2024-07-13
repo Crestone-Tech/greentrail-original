590174bbb65a43d872b77be954069709daafa996
cuhrmunken <elliott.davis@crestonetech.com>
2024-07-13 13:26:17 -0700

    adds more towns for BC

--- seeds/town-seeds-canada.js
+++ seeds/town-seeds-canada.js
@@ -2,11 +2,39 @@ const { Town } = require("../models/Location");
 
 const townData = [
   {
+    town_name: "Creston",
+    province_id: 2,
+  },
+  {
+    town_name: "Fernie",
+    province_id: 2,
+  },
+  {
     town_name: "Nelson",
     province_id: 2,
   },
   {
+    town_name: "Penticton",
+    province_id: 2,
+  },
+  {
     town_name: "Revelstoke",
+    province_id: 2,
+  },
+  {
+    town_name: "Squamish",
+    province_id: 2,
+  },
+  {
+    town_name: "Tofino",
+    province_id: 2,
+  },
+  {
+    town_name: "Victoria",
+    province_id: 2,
+  },
+  {
+    town_name: "Whistler",
     province_id: 2,
   },
 ];
