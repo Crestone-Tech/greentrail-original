0410cbca777ab82ba14b222a4c1609a9da67f740
cuhrmunken <elliott.davis@crestonetech.com>
2024-07-12 20:25:24 -0700

    calls country seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,14 @@
+const seedCountries = require('./country-seeds');
+
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
     console.log();
     console.log("Seeds: start");
     await sequelize.sync({force: true});
+    console.log('\n----- DATABASE SYNCED -----\n');
+
+    await seedCountries();
 
 
     console.log("Seeds: done");
