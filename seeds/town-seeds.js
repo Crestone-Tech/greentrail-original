8e0f8fb52a67a8d4594277007c336441b0da0195
cuhrmunken <elliott.davis@crestonetech.com>
2024-07-13 13:03:34 -0700

    adds town seeds for Jalisco

--- seeds/town-seeds.js
+++ seeds/town-seeds.js
@@ -29,6 +29,54 @@ const townData = [
     town_name: "Santa Maria del Oro",
     province_id: 31,
   },
+  {
+    town_name: "San Sebastián del Oeste",
+    province_id: 26,
+  },
+  {
+    town_name: "Mascota",
+    province_id: 26,
+  },
+  {
+    town_name: "Talpa de Allende",
+    province_id: 26,
+  },
+  {
+    town_name: "Tequila",
+    province_id: 26,
+  },
+  {
+    town_name: "Mazamitla",
+    province_id: 26,
+  },
+  {
+    town_name: "Tapalpa",
+    province_id: 26,
+  },
+  {
+    town_name: "Lagos de Moreno",
+    province_id: 26,
+  },
+  {
+    town_name: "Chapala",
+    province_id: 26,
+  },
+  {
+    town_name: "Ajijic",
+    province_id: 26,
+  },
+  {
+    town_name: "Cocula",
+    province_id: 26,
+  },
+  {
+    town_name: "Sayula",
+    province_id: 26,
+  },
+  {
+    town_name: "Temacapulín",
+    province_id: 26,
+  },
 ];
 
 const seedTown = () => Town.bulkCreate(townData);
