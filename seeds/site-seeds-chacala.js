a08c709399097dadc6235718b8e2b723d4b825cc
cuhrmunken <elliott.davis@crestonetech.com>
2024-07-14 07:12:55 -0700

    adds generated rows

--- seeds/site-seeds-chacala.js
+++ seeds/site-seeds-chacala.js
@@ -33,6 +33,30 @@ const siteData = [
     longitude: null,
     map_link: "https://maps.app.goo.gl/hTLMkCEziD2ves5c9",
   },
+  {
+    site_name: "Lalaxtli",
+    town_id: 1,
+    street_address: "Av. Santa Socorro 100",
+    latitude: "21.16580257750813",
+    longitude: "-105.22716496251479",
+    map_link: "https://maps.app.goo.gl/AdTUpNqDd7mxP22p9",
+  },
+  {
+    site_name: "Sewage Ponds of Las Varas",
+    town_id: 1,
+    street_address: "",
+    latitude: "21.178415331247148",
+    longitude: "-105.15212022196012",
+    map_link: "",
+  },
+  {
+    site_name: "Cafe Palmar",
+    town_id: 1,
+    street_address: "Av. Islas Canarias 7",
+    latitude: "21.165148833429846",
+    longitude: "-105.22431923367543",
+    map_link: "https://maps.app.goo.gl/z3RTmQBQJQF2m2W97",
+  },
 ];
 
 const seedSite = () => Site.bulkCreate(siteData);
