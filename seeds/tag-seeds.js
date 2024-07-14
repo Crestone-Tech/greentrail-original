2f9e594a60d988717dc22f421e8b1b74788a4118
cuhrmunken <elliott.davis@crestonetech.com>
2024-07-14 12:57:19 -0700

    adds all tag seeds

--- seeds/tag-seeds.js
+++ seeds/tag-seeds.js
@@ -5,10 +5,37 @@ const tagData = [
     tag_name: "eat",
   },
   {
+    tag_name: "coffee",
+  },
+  {
     tag_name: "stay",
   },
   {
     tag_name: "explore",
+  },
+  {
+    tag_name: "amenity",
+  },
+  {
+    tag_name: "groceries",
+  },
+  {
+    tag_name: "massage",
+  },
+  {
+    tag_name: "learn",
+  },
+  {
+    tag_name: "language_lessons",
+  },
+  {
+    tag_name: "cooking_lessons",
+  },
+  {
+    tag_name: "history",
+  },
+  {
+    tag_name: "flora_and_fauna",
   },
 ];
 
