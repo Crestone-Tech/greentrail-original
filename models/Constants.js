7ebb8dac4afa0db9617f9d41f986550971f11568
cuhrmunken <elliott.davis@crestonetech.com>
2024-07-14 08:27:49 -0700

    adds Provider seeds

--- models/Constants.js
+++ models/Constants.js
@@ -1,8 +1,9 @@
 const TABLES = {
+  COMMUNITY: "greentrail_db_community",
   COUNTRY: "greentrail_db_country",
   PROVINCE: "greentrail_db_province",
-  TOWN: "greentrail_db_town",
   SITE: "greentrail_db_site",
+  TOWN: "greentrail_db_town",
 };
 
 module.exports = { TABLES }
