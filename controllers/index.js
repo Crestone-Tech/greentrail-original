bbfdea96c48f12b453777c19dd0b4952e5766459
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-07-15 12:24:12 -0400

    feat: login page with clickable buttons

--- controllers/index.js
+++ controllers/index.js
@@ -5,11 +5,13 @@ const router = require("express").Router();
 const homeRoutes = require("./home-routes.js");
 const locationRoutes = require("./location-routes.js");
 const communityRoutes = require("./community-routes.js");
+const loginRoutes = require("./login-routes.js");
 //const apiRoutes = require("./api");
 
 router.use("/", homeRoutes);
 router.use("/locations", locationRoutes);
 router.use("/community", communityRoutes);
+router.use("/login", loginRoutes);
 //router.use("/api", apiRoutes);
 
 /* EXPORTS */
--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,5 @@
 /* VARIABLES */
+/* Variables for homepage */
 const subtitle = document.querySelector("#homepage-subtitle");
 const subtitleText = [
   "Travel respectfully.",
@@ -7,6 +8,12 @@ const subtitleText = [
 ];
 let subtitleIndex = 0;
 
+/* Variables for login page */
+const signInButton = document.getElementById("sign-in-button");
+const signUpButton = document.getElementById("sign-up-button");
+const signInSection = document.getElementById("sign-in");
+const signUpSection = document.getElementById("sign-up");
+
 /* FUNCTIONS */
 /* Change subtitle of homepage every 8.2 seconds */
 function changeText() {
@@ -14,8 +21,25 @@ function changeText() {
   subtitleIndex = (subtitleIndex + 1) % subtitleText.length;
 }
 
+/* Show sign in and hide sign up */
+function showSignIn() {
+  signInSection.style.display = "block";
+  signUpSection.style.display = "none";
+}
+
+/* Show sign up and hide sign in */
+function showSignUp() {
+  signInSection.style.display = "none";
+  signUpSection.style.display = "block";
+}
+
 /* INITIALIZERS */
 window.onload = () => {
-  changeText;
+  showSignIn();
+  changeText();
   setInterval(changeText, 8200);
 };
+
+/* EVENT LISTENERS */
+signInButton.addEventListener("click", showSignIn);
+signUpButton.addEventListener("click", showSignUp);
