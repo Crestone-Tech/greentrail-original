a46a18e48245a687eaf3e5922689cad2d4da4f95
cuhrmunken <elliott.davis@crestonetech.com>
2024-07-15 07:47:50 -0700

    changed table ref for tag_id, added provider tag seeds

--- seeds/tag-seeds.js
+++ seeds/tag-seeds.js
@@ -2,7 +2,40 @@ const Tag = require("../models/Tag");
 
 const tagData = [
   {
+    tag_name: "amenity",
+  },
+  {
+    tag_name: "coffee",
+  },
+  {
+    tag_name: "cooking_lessons",
+  },
+  {
     tag_name: "eat",
+  },
+  {
+    tag_name: "explore",
+  },
+  {
+    tag_name: "flora_and_fauna",
+  },
+  {
+    tag_name: "groceries",
+  },
+  {
+    tag_name: "history",
+  },
+  {
+    tag_name: "language_lessons",
+  },
+  {
+    tag_name: "learn",
+  },
+  {
+    tag_name: "massage",
+  },
+  {
+    tag_name: "stay",
   },
 ];
 
