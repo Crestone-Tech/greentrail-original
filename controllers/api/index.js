ff13ef2f2a3c6719e07a33a62c17b8aec2011e49
Adam Mathis <adam.mathis05@gmail.com>
2024-07-15 13:42:09 -0500

    updated index file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,16 +5,15 @@ const router = require("express").Router();
 const locationRoutes = require("./location-routes.js");
 const eventRoutes = require("./event-routes.js");
 const eventSeriesRoutes = require("./eventSeries-routes.js");
+const communityRoutes = require("./community-routes.js");
+const userRoutes = require("./user-routes");
 
 router.use("/locations", locationRoutes);
 router.use("/events", eventRoutes);
 router.use("/eventSeries", eventSeriesRoutes);
-const communityRoutes= require('./community-routes.js');
-const userRoutes = require("./user-routes");
-
 router.use("/locations", locationRoutes);
 router.use("/users", userRoutes);
-router.use('/communities', communityRoutes)
+router.use("/communities", communityRoutes);
 
 /* EXPORTS */
 module.exports = router;
