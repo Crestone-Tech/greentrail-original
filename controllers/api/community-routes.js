5075d34f7b296fce8bc6af7e35894f05097fb5b9
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-07-16 12:01:10 -0400

    change community to community/name, add partials

--- controllers/api/community-routes.js
+++ controllers/api/community-routes.js
@@ -1,31 +1,34 @@
+/* DEPENDENCIES */
 const router = require("express").Router();
 const sequelize = require("../../config/connection");
 const { Country, Province, Town, Site, Community } = require("../../models");
- 
-// Route to get listing of all communities
-router.get('/', async (req,res)=> {
-    try{
-        const communityData = await Community.findAll();
-        res.status(200).json(communityData);
 
-    }catch(err) {
-        res.status(500).json(err);
-    }
+/* ROUTES */
+/* Route to get listing of all communities */
+router.get("/", async (req, res) => {
+  try {
+    const communityData = await Community.findAll();
+    res.status(200).json(communityData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-//Route to get community by id, WIP
-router.get('community/:id', async (req,res) => {
-    try{ 
-      const townData = await Community.findByPk(req.params.id, {
-        include:[{model:Site, model: Town}]
-      });
-      if(!townData){
-        res.status(404).json({message:"No such town exists as community"})
-      }
-      res.status(200).json(townData)
-    } catch(err){
-      res.status(500).json(err)
-    }
-  });
-  
-  module.exports = router;
+/* Route to get community by name, WIP */
+// router.get("/community/:name", async (req, res) => {
+//   try {
+//     const communityData = await Community.findOne({
+//       where: { community_name: req.params.name },
+//       include: [{ model: Site, model: Town }],
+//     });
+//     if (!communityData) {
+//       res.status(404).json({ message: "No such town exists as community" });
+//     }
+//     res.status(200).json(communityData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+/* EXPORTS */
+module.exports = router;
