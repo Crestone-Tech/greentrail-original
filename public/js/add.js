bfafb8247157657d27286b54f5fe82f518539a9e
cuhrmunken <elliott.davis@crestonetech.com>
2024-07-16 14:49:43 -0700

    handleSaveServiceProvider require all fields

--- public/js/add.js
+++ public/js/add.js
@@ -1,9 +1,52 @@
 const addServiceModal = $("#addServiceModal");
 const saveNewServiceProviderBtn = $("#saveNewServiceProviderBtn");
 
-function handleSaveServiceProvider() {
-    const providerTitleService = $('#');
-    alert('foo');
+const userSignIn = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector("#sign-in-email").value;
+  const password = document.querySelector("#sign-in-password").value;
+
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to log in.");
+    }
+  }
+};
+
+
+
+const handleSaveServiceProvider = async (event) => {
+    const serviceCommunityInput = $("#serviceCommunityInput");
+    const serviceNameInput = $("#serviceNameInput");
+    const serviceTypeInput = $("#serviceTypeInput");
+    const serviceDescriptionInput = $("#serviceDescriptionInput");
+
+    if (
+        !(
+            serviceCommunityInput.val() &&
+            serviceDescriptionInput.val() &&
+            serviceNameInput.val() &&
+            serviceTypeInput.val()
+        )
+    ) {
+        alert("All fields are required");
+        return;
+    }
+    console.log("serviceCommunityInput", serviceCommunityInput.val());
+    console.log("serviceDescriptionInput", serviceDescriptionInput.val());
+    console.log("serviceNameInput", serviceNameInput.val());
+    console.log("serviceTypeInputa", serviceTypeInput.val());
+
+
 }
 
 saveNewServiceProviderBtn.on('click', handleSaveServiceProvider);
