585fddec8609ffe9bbce7b8992f9ef1a108c0fcc
cuhrmunken <elliott.davis@crestonetech.com>
2024-07-16 15:17:51 -0700

    adds post route

--- controllers/api/provider-routes.js
+++ controllers/api/provider-routes.js
@@ -4,10 +4,21 @@ const sequelize = require("../../config/connection");
 const { Provider } = require("../../models");
 
 /* ROUTES */
-/* Route to get listing of all providers */
+/* get listing of all providers */
 router.get("/", async (req, res) => {
   try {
     const providerData = await Provider.findAll();
+    res.status(200).json(providerData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+/* POST create a Provider */
+router.post("/", async (req, res) => {
+  try {
+    console.log(req.body);
+    const providerData = await Provider.create(req.body);
     res.status(200).json(providerData);
   } catch (err) {
     res.status(500).json(err);
