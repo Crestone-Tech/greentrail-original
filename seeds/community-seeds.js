8309ecb5f2477f0c245c2c4923a7328a30650858
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-07-17 16:38:22 -0400

    add description to community

--- seeds/community-seeds.js
+++ seeds/community-seeds.js
@@ -1,8 +1,9 @@
-const Community = require('../models/Community');
+const Community = require("../models/Community");
 
 const communityData = [
   {
     community_name: "Chacala",
+    description: "Lorem ipsum",
     town_id: 1,
     community_center_site_id: 2,
   },
